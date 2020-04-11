d2fc7d203fa687459ab4c57f4b0da741610585f9
huai99 <edricgan.44@gmail.com>
2020-04-11 21:29:47 +0800

    Add kafka log appender to the project

--- src/main/java/com/example/siehuai/kafkatesting/controller/KafkaController.java
+++ src/main/java/com/example/siehuai/kafkatesting/controller/KafkaController.java
@@ -3,6 +3,8 @@ package com.example.siehuai.kafkatesting.controller;
 import com.example.siehuai.kafkatesting.kafka.Producer;
 import com.example.siehuai.kafkatesting.repository.Person;
 import com.example.siehuai.kafkatesting.service.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KafkaController {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaController.class);
 
     @Autowired
     Producer producer;
@@ -34,6 +38,7 @@ public class KafkaController {
 
     @GetMapping(value = "/publishPerson")
     public Integer publishPerson(@RequestParam("value") String name) {
+        logger.info("Start publishing person");
         Person person = new Person();
         person.setName(name);
         person.setCity("KL");
